37f671fe81c0e034568643bfaf22f8927e915f1d
Subbu Devasani <56950679+SubbuDevasani@users.noreply.github.com>
2022-01-03 16:02:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-var feed=[];
+var div = document.getElementById("result");
+var feed = [];
+
 function addFeedback(){
-  var x=document.getElementById("feedback").ariaValueMax;
+  var x=document.getElementById("feedback").value;
   feed.push(x);
+  console.log(feed)
+  div.innerHTML= "Successfully Added Feedback Details!";
 }
 
 function displayFeedback(){
   var i=0;
   var res="";
-  res=res="Feedback"+(i+1)+"<br/>+feed[i]+"<br/>";
-
-  document.getElementsByName("inner")[1].innerHTML=res;
-  feed=[];
+  for(var i=0;i<feed.length;i++){ 
+  res = res + "Feedback "+(i+1)+"<br/>"+feed[i]+"<br/>";
+   div.innerHTML = res;
+  }
 }
-
